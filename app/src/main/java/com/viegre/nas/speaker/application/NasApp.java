ca491124038f2d9af7b2f6001a4af9219e5680b4
Djangoogle <Djangoogle@foxmail.com>
2020-11-23 11:33:56 +0800

    优化代码

--- app/src/main/java/com/viegre/nas/speaker/application/NasApp.java
+++ app/src/main/java/com/viegre/nas/speaker/application/NasApp.java
@@ -3,7 +3,6 @@ package com.viegre.nas.speaker.application;
 import android.app.Application;
 
 import com.blankj.utilcode.util.Utils;
-import com.viegre.nas.voice_wake_up.util.WakeRecognition;
 
 /**
  * Created by Djangoogle on 2020/09/10 10:21 with Android Studio.
@@ -14,6 +13,5 @@ public class NasApp extends Application {
 	public void onCreate() {
 		super.onCreate();
 		Utils.init(this);
-		WakeRecognition.INSTANCE.initialize(this);
 	}
 }
