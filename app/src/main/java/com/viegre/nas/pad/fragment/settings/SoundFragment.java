ebcc63165ed994bc59403a9fb510284d986e3b35
Djangoogle <Djangoogle@foxmail.com>
2021-03-08 15:39:51 +0800

    # 添加音量调节页面

--- app/src/main/java/com/viegre/nas/pad/fragment/settings/SoundFragment.java
+++ app/src/main/java/com/viegre/nas/pad/fragment/settings/SoundFragment.java
@@ -1,5 +1,9 @@
 package com.viegre.nas.pad.fragment.settings;
 
+import android.media.AudioManager;
+import android.widget.SeekBar;
+
+import com.blankj.utilcode.util.VolumeUtils;
 import com.djangoogle.framework.fragment.BaseFragment;
 import com.viegre.nas.pad.databinding.FragmentSoundBinding;
 
@@ -9,7 +13,24 @@ import com.viegre.nas.pad.databinding.FragmentSoundBinding;
 public class SoundFragment extends BaseFragment<FragmentSoundBinding> {
 
 	@Override
-	protected void initialize() {}
+	protected void initialize() {
+		int maxVolume = VolumeUtils.getMaxVolume(AudioManager.STREAM_MUSIC);
+		mViewBinding.acsbSoundVolumeControl.setMax(maxVolume);
+		int volume = VolumeUtils.getVolume(AudioManager.STREAM_MUSIC);
+		mViewBinding.acsbSoundVolumeControl.setProgress(volume);
+		mViewBinding.acsbSoundVolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+				VolumeUtils.setVolume(AudioManager.STREAM_MUSIC, i, AudioManager.FLAG_PLAY_SOUND);
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}
+		});
+	}
 
 	public static SoundFragment newInstance() {
 		return new SoundFragment();
