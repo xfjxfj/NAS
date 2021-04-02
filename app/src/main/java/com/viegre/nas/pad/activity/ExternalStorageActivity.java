f130a6b2057296a09bef13ed08fc2b4a59fb5b34
Djangoogle <Djangoogle@foxmail.com>
2021-04-02 15:20:15 +0800

    # 添加外部存储列表

--- app/src/main/java/com/viegre/nas/pad/activity/ExternalStorageActivity.java
+++ app/src/main/java/com/viegre/nas/pad/activity/ExternalStorageActivity.java
@@ -1,7 +1,17 @@
 package com.viegre.nas.pad.activity;
 
+import android.graphics.Color;
+
 import com.djangoogle.framework.activity.BaseActivity;
+import com.viegre.nas.pad.adapter.ExternalStorageAdapter;
 import com.viegre.nas.pad.databinding.ActivityExternalStorageBinding;
+import com.viegre.nas.pad.entity.ExternalStorageEntity;
+import com.yqritc.recyclerviewflexibledivider.VerticalDividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 /**
  * 外部存储
@@ -9,8 +19,26 @@ import com.viegre.nas.pad.databinding.ActivityExternalStorageBinding;
  */
 public class ExternalStorageActivity extends BaseActivity<ActivityExternalStorageBinding> {
 
+	private ExternalStorageAdapter mExternalStorageAdapter;
+
 	@Override
 	protected void initialize() {
+		mViewBinding.acivExternalStorageBack.setOnClickListener(view -> finish());
+		initList();
+	}
 
+	private void initList() {
+		mExternalStorageAdapter = new ExternalStorageAdapter();
+		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+		linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+		mViewBinding.rvExternalStorageList.setLayoutManager(linearLayoutManager);
+		mViewBinding.rvExternalStorageList.addItemDecoration(new VerticalDividerItemDecoration.Builder(mActivity).color(Color.TRANSPARENT)
+		                                                                                                         .size(30)
+		                                                                                                         .build());
+		mViewBinding.rvExternalStorageList.setAdapter(mExternalStorageAdapter);
+		List<ExternalStorageEntity> list = new ArrayList<>();
+		list.add(new ExternalStorageEntity("外部磁盘", ""));
+		list.add(new ExternalStorageEntity("雨人的U盘", ""));
+		mExternalStorageAdapter.setList(list);
 	}
 }
