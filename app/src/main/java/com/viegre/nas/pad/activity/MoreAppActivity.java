e04cdeae113bc3a724d7a80ec26d0dde83f8167c
topqizhi <1004028993@qq.com>
2021-04-15 10:13:18 +0800

    添加更多页面

---
+++ app/src/main/java/com/viegre/nas/pad/activity/MoreAppActivity.java
@@ -0,0 +1,12 @@
+package com.viegre.nas.pad.activity;
+
+
+import com.djangoogle.framework.activity.BaseActivity;
+import com.viegre.nas.pad.R;
+
+public class MoreAppActivity extends BaseActivity {
+    @Override
+    protected void initialize() {
+
+    }
+}
