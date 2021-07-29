d7cbbf22eb5c7919f88fa2b2d24d2a63cc29ec30
xufangjie <1004028993@qq.com>
2021-07-29 11:20:37 +0800

    修复系统应用无法调用webview

--- app/src/main/java/com/viegre/nas/pad/activity/WebActivity.java
+++ app/src/main/java/com/viegre/nas/pad/activity/WebActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -12,12 +13,18 @@ import android.webkit.WebViewClient;
 
 import com.viegre.nas.pad.R;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class WebActivity extends AppCompatActivity {
+    private static final String TAG = "WebActivity";
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hookWebView();
         setContentView(R.layout.activity_web);
         webView = findViewById(R.id.webview);
         findViewById(R.id.webview_back).setOnClickListener(new View.OnClickListener() {
@@ -45,19 +52,19 @@ public class WebActivity extends AppCompatActivity {
                 //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
                 //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
 
-                if (url.toString().contains("sina.cn")){
+                if (url.toString().contains("sina.cn")) {
                     view.loadUrl("http://ask.csdn.net/questions/178242");
                     return true;
                 }
                 return false;
             }
+
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
-            {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
                 //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    if (request.getUrl().toString().contains("sina.cn")){
+                    if (request.getUrl().toString().contains("sina.cn")) {
                         view.loadUrl("http://ask.csdn.net/questions/178242");
                         return true;
                     }
@@ -69,5 +76,60 @@ public class WebActivity extends AppCompatActivity {
         });
 
         webView.loadUrl("http://news.baidu.com");//加载url
+    }
+
+
+    public static void hookWebView(){
+        int sdkInt = Build.VERSION.SDK_INT;
+        try {
+            Class<?> factoryClass = Class.forName("android.webkit.WebViewFactory");
+            Field field = factoryClass.getDeclaredField("sProviderInstance");
+            field.setAccessible(true);
+            Object sProviderInstance = field.get(null);
+            if (sProviderInstance != null) {
+                Log.i(TAG,"sProviderInstance isn't null");
+                return;
+            }
+            Method getProviderClassMethod;
+            if (sdkInt > 22) {
+                getProviderClassMethod = factoryClass.getDeclaredMethod("getProviderClass");
+            } else if (sdkInt == 22) {
+                getProviderClassMethod = factoryClass.getDeclaredMethod("getFactoryClass");
+            } else {
+                Log.i(TAG,"Don't need to Hook WebView");
+                return;
+            }
+            getProviderClassMethod.setAccessible(true);
+            Class<?> factoryProviderClass = (Class<?>) getProviderClassMethod.invoke(factoryClass);
+            Class<?> delegateClass = Class.forName("android.webkit.WebViewDelegate");
+            Constructor<?> delegateConstructor = delegateClass.getDeclaredConstructor();
+            delegateConstructor.setAccessible(true);
+            if(sdkInt < 26){//低于Android O版本
+                Constructor<?> providerConstructor = factoryProviderClass.getConstructor(delegateClass);
+                if (providerConstructor != null) {
+                    providerConstructor.setAccessible(true);
+                    sProviderInstance = providerConstructor.newInstance(delegateConstructor.newInstance());
+                }
+            } else {
+                Field chromiumMethodName = factoryClass.getDeclaredField("CHROMIUM_WEBVIEW_FACTORY_METHOD");
+                chromiumMethodName.setAccessible(true);
+                String chromiumMethodNameStr = (String)chromiumMethodName.get(null);
+                if (chromiumMethodNameStr == null) {
+                    chromiumMethodNameStr = "create";
+                }
+                Method staticFactory = factoryProviderClass.getMethod(chromiumMethodNameStr, delegateClass);
+                if (staticFactory!=null){
+                    sProviderInstance = staticFactory.invoke(null, delegateConstructor.newInstance());
+                }
+            }
+            if (sProviderInstance != null){
+                field.set("sProviderInstance", sProviderInstance);
+                Log.i(TAG,"Hook success!");
+            } else {
+                Log.i(TAG,"Hook failed!");
+            }
+        } catch (Throwable e) {
+            Log.w(TAG,e);
+        }
     }
 }
